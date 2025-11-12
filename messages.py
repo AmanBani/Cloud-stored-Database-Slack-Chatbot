71e575fbbfd38943fab19841669f18ff56f604d1
Aman Bani <amanbani647@gmail.com>
2025-11-12 23:43:08 +0530

    Updated Messager Response

--- messages.py
+++ messages.py
@@ -16,3 +16,20 @@ def create_error_message(message: str, details: Optional[Any] = None) -> Dict[st
         
     logger.debug(f"Created error response : {message}")
     return response
+
+
+def create_success_response(data: Any, message: Optional[str] = None) -> Dict[str, Any];
+    response = {
+        "type":"success",
+        "data":data
+    }
+    
+    if message:
+        response["mesage"] = message
+    
+    logger.debug("Created Success response")
+    return response
+
+
+def validate_event(body: Dict, user_manage) -> Union[Dict[str,str], None]:
+    
